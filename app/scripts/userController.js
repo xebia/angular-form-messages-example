f068cb5e407ee30a476786d6d09f3ea3bafd2296
Frank van Wijk <fvanwijk@xebia.com>
2015-07-17 10:31:35 +0200

    Add timeout to submit callback so that it is more realistic

--- app/scripts/userController.js
+++ app/scripts/userController.js
@@ -1,16 +1,18 @@
-angular.module('angularFormMessagesExample').controller('UserController', function ($q, $scope) {
+angular.module('angularFormMessagesExample').controller('UserController', function ($q, $scope, $timeout) {
   $scope.user = {};
 
   $scope.afterSubmit = function () {
-    return $q.reject({
-      validation: {
-        address: ['Cannot locate address geo'],
-        'user.name': ['User name server side error'],
-        'user.email': [
-          'User email server side error',
-          'User email server side error 2'
-        ]
-      }
-    });
+    return $timeout(function () {
+      return $q.reject({
+        validation: {
+          address: ['Cannot locate address geo'],
+          'user.name': ['User name server side error'],
+          'user.email': [
+            'User email server side error',
+            'User email server side error 2'
+          ]
+        }
+      });
+    }, 2000);
   };
 });
