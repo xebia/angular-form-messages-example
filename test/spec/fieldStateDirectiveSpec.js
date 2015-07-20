86f6ff45f9cdd93e189a2d37dd6fc34c2c76c77c
Frank van Wijk <fvanwijk@xebia.com>
2015-07-20 17:26:15 +0200

    Rewritten fieldStateDirective so that it shows the correct class and removes all the other classes. The 'has-success' is only added when there are no messages and "showSuccess" is true, or when there is a success message. When there are multiple messages with different types, the class for the highest severity is added.

--- test/spec/fieldStateDirectiveSpec.js
+++ test/spec/fieldStateDirectiveSpec.js
@@ -1,10 +1,26 @@
 describe('afFieldWrap', function () {
 
-  function setup(invalid, fieldName) {
-    this.element.removeClass(invalid ? 'has-error' : 'has-success');
-    this.element.addClass(invalid ? 'has-success' : 'has-error');
-    $rootScope.$broadcast('validation', fieldName, invalid ? ['Error'] : []);
+  function sendValidation(fieldName, messages) {
+    $rootScope.$broadcast('validation', fieldName, messages);
     this.$scope.$digest();
+  }
+
+  function setup(messageType, fieldName) {
+    if (messageType) {
+      this.element.removeClass('has-' + messageType);
+    }
+    this.element.addClass(messageType === 'error' ? 'has-warning has-info has-success' : 'has-error');
+
+    sendValidation.call(this, fieldName, messageType ? [{ message: 'Error', type: messageType }] : []);
+  }
+
+  function errorWarningSetup() {
+    this.element.addClass('has-warning');
+
+    sendValidation.call(this, 'user.name', [
+      { message: 'Error', type: 'error' },
+      { message: 'Warning', type: 'warning' }
+    ]);
   }
 
   function checkErrorClass(className, invert) {
@@ -17,12 +33,22 @@ describe('afFieldWrap', function () {
 
   var
     $rootScope,
-    invalidSetup = _.partial(setup, true),
-    validSetup = _.partial(setup, false),
-    expectHasSuccess = _.partial(checkErrorClass, 'has-success'),
+
+    errorSetup = _.partial(setup, 'error'),
+    warningSetup = _.partial(setup, 'warning'),
+    infoSetup = _.partial(setup, 'info'),
+    successSetup = _.partial(setup, 'success'),
+    noMessageSetup = _.partial(setup, false),
+
     expectHasError = _.partial(checkErrorClass, 'has-error'),
-    expectHasNoSuccess = _.partial(checkErrorClass, 'has-success', true),
-    expectHasNoError = _.partial(checkErrorClass, 'has-error', true);
+    expectHasWarning = _.partial(checkErrorClass, 'has-warning'),
+    expectHasInfo = _.partial(checkErrorClass, 'has-info'),
+    expectHasSuccess = _.partial(checkErrorClass, 'has-success'),
+
+    expectHasNoError = _.partial(checkErrorClass, 'has-error', true),
+    expectHasNoWarning = _.partial(checkErrorClass, 'has-warning', true),
+    expectHasNoInfo = _.partial(checkErrorClass, 'has-info', true),
+    expectHasNoSuccess = _.partial(checkErrorClass, 'has-success', true);
 
   beforeEach(function () {
     mox
@@ -51,19 +77,48 @@ describe('afFieldWrap', function () {
         beforeEach(_.partial(showSuccess, true));
 
         describe('when the validation is "valid"', function () {
-          beforeEach(_.partial(validSetup, 'user.name'));
-
-          it('should remove the "has-error" class from the element', expectHasNoError);
+          beforeEach(_.partial(noMessageSetup, 'user.name'));
 
           it('should add the "has-success" class to the element', expectHasSuccess);
+          it('should remove the "has-error" class from the element', expectHasNoError);
+          it('should remove the "has-warning" class from the element', expectHasNoWarning);
+          it('should remove the "has-info" class from the element', expectHasNoInfo);
         });
 
-        describe('when the validation is "invalid"', function () {
-          beforeEach(_.partial(invalidSetup, 'user.name'));
+        describe('when the validation is "error"', function () {
+          beforeEach(_.partial(errorSetup, 'user.name'));
 
           it('should add a "has-error" class to the element', expectHasError);
-
+          it('should remove the "has-warning" class from the element', expectHasNoWarning);
+          it('should remove the "has-info" class from the element', expectHasNoInfo);
           it('should remove the "has-success" class from the element', expectHasNoSuccess);
+        });
+
+        describe('when the validation is "warning"', function () {
+          beforeEach(_.partial(warningSetup, 'user.name'));
+
+          it('should add a "has-warning" class to the element', expectHasWarning);
+          it('should remove the "has-error" class from the element', expectHasNoError);
+          it('should remove the "has-info" class from the element', expectHasNoInfo);
+          it('should remove the "has-success" class from the element', expectHasNoSuccess);
+        });
+
+        describe('when the validation is "info"', function () {
+          beforeEach(_.partial(infoSetup, 'user.name'));
+
+          it('should add a "has-warning" class to the element', expectHasInfo);
+          it('should remove the "has-error" class from the element', expectHasNoError);
+          it('should remove the "has-warning" class from the element', expectHasNoWarning);
+          it('should remove the "has-success" class from the element', expectHasNoSuccess);
+        });
+
+        describe('when the validation is "success"', function () {
+          beforeEach(_.partial(successSetup, 'user.name'));
+
+          it('should add a "has-success" class to the element', expectHasSuccess);
+          it('should remove the "has-error" class from the element', expectHasNoError);
+          it('should remove the "has-warning" class from the element', expectHasNoWarning);
+          it('should remove the "has-info" class from the element', expectHasNoInfo);
         });
       });
 
@@ -71,28 +126,41 @@ describe('afFieldWrap', function () {
         beforeEach(_.partial(showSuccess, false));
 
         describe('when the validation is "valid"', function () {
-          beforeEach(_.partial(validSetup, 'user.name'));
+          beforeEach(_.partial(noMessageSetup, 'user.name'));
 
           it('should not add the "has-success" class', expectHasNoSuccess);
         });
 
-        describe('when the validation is "invalid"', function () {
-          beforeEach(_.partial(invalidSetup, 'user.name'));
+        describe('when the validation is "error"', function () {
+          beforeEach(_.partial(errorSetup, 'user.name'));
 
-          it('should not remove the "has-success" class', expectHasSuccess);
+          it('should remove the "has-success" class', expectHasNoSuccess);
         });
+
+        describe('when the validation is "success"', function () {
+          beforeEach(_.partial(successSetup, 'user.name'));
+
+          it('should add the "has-success" class', expectHasSuccess);
+        });
+      });
+
+      describe('when there are two messages', function () {
+        beforeEach(_.partial(errorWarningSetup));
+
+        it('should add the class with the highest severity', expectHasError);
+        it('should not add the class with the lower severity', expectHasNoWarning);
       });
     });
 
     describe('when it is not meant for this field wrap', function () {
 
       it('should not add the "has-error" class', function () {
-        invalidSetup.call(this, 'user.other');
+        errorSetup.call(this, 'user.other');
         expectHasNoError.call(this);
       });
 
       it('should not remove the "has-error" class', function () {
-        validSetup.call(this, 'user.other');
+        noMessageSetup.call(this, 'user.other');
         expectHasError.call(this);
       });
     });
