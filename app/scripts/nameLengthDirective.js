4165d1a2edeed82163350260478bc533eda8a374
Frank van Wijk <fvanwijk@xebia.com>
2015-07-20 13:06:30 +0200

    added nameLength directive which validates the length of the name field and sets a warning if invalid

---
+++ app/scripts/nameLengthDirective.js
@@ -0,0 +1,20 @@
+angular.module('angularFormMessagesExample')
+  // Custom directive for showing a warning when the field is
+  // ng-min-length could not be used because that shows an error instead of warning
+  .directive('nameLength', function () {
+    return {
+      restrict: 'A',
+      require: ['ngModel', 'afField'],
+      link: function ($scope, element, attrs, ctrls) {
+        var ngModelCtrl = ctrls[0];
+        var afFieldCtrl = ctrls[1];
+
+        ngModelCtrl.$validators.nameLength = function (modelValue, viewValue) {
+          var value = modelValue || viewValue;
+          var isValid = ngModelCtrl.$isEmpty(value) || value.length >= attrs.nameLength;
+          afFieldCtrl.messageType = isValid ? 'error' : 'warning';
+          return isValid;
+        };
+      }
+    };
+  });
