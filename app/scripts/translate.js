155d3857a8c367eb3743cfb71a17d7ad7ce36d9e
Frank van Wijk <fvanwijk@xebia.com>
2015-08-03 15:00:19 +0200

    Created a translateService that helps with generic and field specific translations. These are saved in the getLabels service.

--- app/scripts/translate.js
+++ app/scripts/translate.js
@@ -2,19 +2,34 @@
  * A simple translate filter and directive for showing messages
  */
 angular.module('angularFormMessagesExample')
-  .filter('translate', function () {
+  .filter('translate', function (
+    TranslateService
+  ) {
+    return function (key) {
+      var translation = TranslateService.getLabel(key);
+      return translation === undefined ? key : translation;
+    };
+  })
+  .factory('TranslateService', function () {
     var translations = {
-      email: 'Please fill in an e-mail address',
+      'userForm.user.email.email': 'Please fill in an e-mail address',
       date: 'Please fill in a valid date',
       period: 'The "from" date must be before the "to" date',
       required: 'This field is required'
     };
 
-    return function (key) {
-      return key in translations ? translations[key] : key;
+    return {
+      getLabel: function (key) {
+        return translations[key];
+      },
+      hasLabel: function (key) {
+        return key in translations;
+      }
     };
   })
-  .directive('translate', function (translateFilter) {
+  .directive('translate', function (
+    translateFilter
+  ) {
     return {
       restrict: 'A',
       link: function ($scope, elem, attr) {
--- test/spec/translateSpec.js
+++ test/spec/translateSpec.js
@@ -1,20 +1,60 @@
-describe('the translate filter', function () {
-  var translateFilter;
+describe('the translateService', function () {
+  var TranslateService;
 
   beforeEach(function () {
     mox
       .module('angularFormMessagesExample')
       .run();
 
+    TranslateService = mox.inject('TranslateService');
+  });
+
+  describe('the getLabel() method', function () {
+    it('should return the translation for a key', function () {
+      expect(TranslateService.getLabel('required')).toBe('This field is required');
+    });
+
+    it('should return undefined when the translate cannot be found', function () {
+      expect(TranslateService.getLabel('not existing')).toBeUndefined();
+    });
+  });
+
+  describe('the hasLabel() method', function () {
+    it('should return true when the label exists', function () {
+      expect(TranslateService.hasLabel('required')).toBe(true);
+    });
+
+    it('should return false when the label no exists', function () {
+      expect(TranslateService.hasLabel('not existing')).toBe(false);
+    });
+  });
+});
+
+describe('the translate filter', function () {
+  var translateFilter;
+
+  beforeEach(function () {
+    mox
+      .module('angularFormMessagesExample')
+      .mockServices('TranslateService')
+      .setupResults(function () {
+        return {
+          TranslateService: { getLabel: 'label' }
+        };
+      })
+      .run();
+
     translateFilter = mox.inject('translateFilter');
   });
 
   it('should return the translation for a key', function () {
-    expect(translateFilter('required')).toBe('This field is required');
+    expect(translateFilter('required')).toBe('label');
   });
 
   it('should return the key when the translate cannot be found', function () {
+    mox.get.TranslateService.getLabel.and.returnValue(undefined);
     expect(translateFilter('not existing')).toBe('not existing');
+    expect(mox.get.TranslateService.getLabel).toHaveBeenCalledWith('not existing');
   });
 });
 
@@ -22,7 +62,10 @@ describe('the translate directive', function () {
   beforeEach(function () {
     mox
       .module('angularFormMessagesExample')
-      .mockServices('translateFilter')
+      .mockServices(
+        'translateFilter',
+        'TranslateService'
+      )
       .setupResults(function () {
         return {
           translateFilter: 'Required translation'
