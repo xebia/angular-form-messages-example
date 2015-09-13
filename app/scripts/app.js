b81e66d21801ae7f3e7f67f2351f9ce3c1f1ad59
Frank van Wijk <fvanwijk@xebia.com>
2015-09-14 00:04:09 +0200

    Added showSuccess example

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,6 +6,7 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/simple', { templateUrl: 'examples/simple.html', name: 'Simple form' })
+      .when('/show-success', { templateUrl: 'examples/showSuccess.html', name: 'Show success' })
       .when('/triggers', { templateUrl: 'examples/triggers.html', name: 'Triggers setting' })
       .when('/repeating-fields', { templateUrl: 'examples/repeatingFields.html', name: 'Repeating fields' })
       .when('/custom', { templateUrl: 'examples/customDirectives.html', name: 'Custom directives' })
